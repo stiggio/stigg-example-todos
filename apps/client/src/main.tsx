f553e47afad929a793dd83aa1c1e4642a9a64842
nadavkaner <nadavkaner1@gmail.com>
2022-10-13 18:31:36 +0300

    fix: stigg theming

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,13 +1,9 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { StiggProvider } from '@stigg/react-sdk';
-import { ThemeProvider } from '@mui/material';
-import CssBaseline from '@mui/material/CssBaseline';
 import { BrowserRouter } from 'react-router-dom';
 import App from './app/app';
-import { theme } from './theme';
 import config from './app/config';
-import { GlobalStyle } from './GlobalStyle';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -24,13 +20,9 @@ root.render(
         },
       }}
     >
-      <ThemeProvider theme={theme}>
-        <BrowserRouter>
-          <CssBaseline />
-          <GlobalStyle />
-          <App />
-        </BrowserRouter>
-      </ThemeProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </StiggProvider>
   </StrictMode>
 );
