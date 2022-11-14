6d66a268696515002d37403386f6dba6bea32925
nadavkaner <nadavkaner1@gmail.com>
2022-11-14 11:02:25 +0200

    feat: use customer portal widget

--- apps/client/src/app/components/pricing/CustomerPortal.tsx
+++ apps/client/src/app/components/pricing/CustomerPortal.tsx
@@ -1,34 +1,11 @@
-import { Card, Grid, Typography } from '@mui/material';
-import { useEffect, useState } from 'react';
+import { Grid } from '@mui/material';
+import { useState } from 'react';
 import { PaywallDialog } from '../paywall/PaywallDialog';
-import { useUser } from '../../hooks/user/useUser';
-import { PlanDetails } from './PlanDetails';
-import { UsageBreakdown } from './UsageBreakdown';
-import { Entitlement, useStiggContext } from '@stigg/react-sdk';
+import { CustomerPortal as StiggCustomerPortal } from '@stigg/react-sdk';
+import { Paywall } from '../paywall/StiggPaywall';
 
 export function CustomerPortal() {
-  const {
-    state: { currentUser },
-  } = useUser();
   const [isPaywallOpen, setIsPaywallOpen] = useState(false);
-  const [entitlements, setEntitlements] = useState<Entitlement[]>([]);
-  const stiggCustomer = currentUser?.stiggCustomer;
-  const { stigg } = useStiggContext();
-
-  useEffect(() => {
-    const getEntitlements = async () => {
-      if (stigg) {
-        const entitlements = await stigg.getEntitlements();
-        setEntitlements(entitlements);
-      }
-    };
-
-    getEntitlements();
-  }, [stigg]);
-
-  if (!stiggCustomer) {
-    return null;
-  }
 
   return (
     <Grid
@@ -36,24 +13,9 @@ export function CustomerPortal() {
       flexDirection="column"
       alignItems="center"
       justifyContent="center"
+      my={3}
     >
-      <Grid item>
-        <Typography variant="h6" fontSize={40} mb={1}>
-          Customer portal
-        </Typography>
-      </Grid>
-      <Grid item width="100%">
-        <Card sx={{ padding: 5 }}>
-          <PlanDetails
-            customer={stiggCustomer}
-            onManagePlan={() => {
-              setIsPaywallOpen(true);
-            }}
-          />
-
-          <UsageBreakdown entitlements={entitlements} />
-        </Card>
-      </Grid>
+      <StiggCustomerPortal paywallComponent={<Paywall />} />
 
       <PaywallDialog
         paywallIsOpen={isPaywallOpen}
